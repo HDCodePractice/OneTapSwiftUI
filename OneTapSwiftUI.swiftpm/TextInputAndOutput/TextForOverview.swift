571584098aef601a71e563e43a3d9774b33e26b5
老房东 <huangdong@gmail.com>
2022-08-28 12:49:10 -0400

    先提交一个版本
    Co-Authored-By: Allenyzh <49468724+Allenyzh@users.noreply.github.com>

---
+++ OneTapSwiftUI.swiftpm/TextInputAndOutput/TextForOverview.swift
@@ -0,0 +1,129 @@
+import SwiftUI
+
+struct TextForOverview: View {
+    var body: some View {
+        TextFontList()
+        Divider()
+        TextFontStyle()
+//        Divider()
+//        TextForFontList()
+    }
+}
+
+struct TextForFontList: View {
+    var body: some View {
+        VStack{
+            Text("Your system font list")
+                .font(.largeTitle)
+            // todo: 解决List不在VStack中显示的问题
+            ScrollView{
+                List{
+                    ForEach(UIFont.familyNames,id:\.self){ name in
+                        HStack{
+                            Text(name)
+                            Text("ABCD1234")
+                                .font(.custom(name, size: 20))
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+private struct TextFontStyle: View {
+    var code = """
+    Text("italic and underline")
+        .italic()
+        .font(.largeTitle)
+        .foregroundColor(.red)
+        .underline(true, color: .black)
+    Text("bold and strikethrough")
+        .bold()
+        .font(.largeTitle)
+        .foregroundColor(.red)
+        .strikethrough(true, color: .blue)
+    Text("system font")
+        .font(.system(size: 30, weight: .regular, design: .serif))
+    Text("11111monospacedDigit")
+        .monospacedDigit()
+        .font(.largeTitle)
+        .foregroundColor(.red)
+    Text("23456monospacedDigit")
+        .monospacedDigit()
+        .font(.largeTitle)
+        .foregroundColor(.red)
+    """
+    var body: some View {
+        VStack{
+            CodePreviewView(code: code)
+            Text("italic and underline")
+                .italic()
+                .font(.largeTitle)
+                .foregroundColor(.red)
+                .underline(true, color: .black)
+            Text("bold and strikethrough")
+                .bold()
+                .font(.largeTitle)
+                .foregroundColor(.red)
+                .strikethrough(true, color: .blue)
+            Text("system font")
+                .font(.system(size: 30, weight: .regular, design: .serif))
+            Text("11111monospacedDigit")
+                .monospacedDigit()
+                .font(.largeTitle)
+                .foregroundColor(.red)
+            Text("23456monospacedDigit")
+                .monospacedDigit()
+                .font(.largeTitle)
+                .foregroundColor(.red)
+        }
+    }
+}
+
+
+private struct TextFontList: View {
+    var code = """
+Text("1234 largeTitle").font(.largeTitle)
+Text("1234 title").font(.title)
+Text("1234 title2").font(.title2)
+Text("1234 title3").font(.title3)
+Text("1234 headline").font(.headline)
+Text("1234 subheadline").font(.subheadline)
+Text("1234 body").font(.body)
+Text("1234 callout").font(.callout)
+Text("1234 footnote").font(.footnote)
+Text("1234 caption").font(.caption)
+Text("1234 caption2").font(.caption2)
+"""
+    
+    var body: some View {
+        VStack{
+            CodePreviewView(code: code)
+            VStack(alignment:.leading){
+                Group{
+                    Text("1234 largeTitle").font(.largeTitle)
+                    Text("1234 title").font(.title)
+                    Text("1234 title2").font(.title2)
+                    Text("1234 title3").font(.title3)
+                    Text("1234 headline").font(.headline)
+                    Text("1234 subheadline").font(.subheadline)
+                    Text("1234 body").font(.body)
+                }
+                Text("1234 callout").font(.callout)
+                Text("1234 footnote").font(.footnote)
+                Text("1234 caption").font(.caption)
+                Text("1234 caption2").font(.caption2)
+            }
+        }
+    }
+}
+
+
+struct TextForOverview_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack{
+            TextForOverview()
+        }
+    }
+}
