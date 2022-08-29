11238202c2a39adf8b445b46ebb11085b1820fe8
老房东 <huangdong@gmail.com>
2022-08-29 07:14:06 -0400

    优化Double Option初始化缺省值

--- OneTapSwiftUI.swiftpm/Model/DoubleOption.swift
+++ OneTapSwiftUI.swiftpm/Model/DoubleOption.swift
@@ -15,6 +15,24 @@ struct DoubleOption: Identifiable{
     var defaultValue : Double?
     var range: ClosedRange<Double>
     
+    init(
+        name: String, 
+        active:Bool=false, 
+        value:Double, 
+        defaultValue: Double? = nil,
+        range: ClosedRange<Double> 
+    ){
+        self.name = name
+        self.active = active
+        self.value = value
+        if let defaultValue=defaultValue {
+            self.defaultValue=defaultValue
+        }else{
+            self.defaultValue=value
+        }
+        self.range = range
+    }
+    
     var valueString : String{
         let formatted = String(format: "%.2f", value)
         return formatted
