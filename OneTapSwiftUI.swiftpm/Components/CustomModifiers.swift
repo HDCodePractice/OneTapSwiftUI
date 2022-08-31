8ecd5fb30eccbc5f8f92786d96801287aa10bc76
老房东 <huangdong@gmail.com>
2022-08-31 12:16:41 -0400

    解决在iPhone中如果系统使用了过大的字体出现折行的问题

--- OneTapSwiftUI.swiftpm/Components/CustomModifiers.swift
+++ OneTapSwiftUI.swiftpm/Components/CustomModifiers.swift
@@ -11,6 +11,8 @@ struct ValueLabel: ViewModifier {
     func body(content: Content) -> some View {
         content
             .frame(width: 60)
+            .lineLimit(1)
+            .minimumScaleFactor(0.5)
             .font(.headline)
             .padding(.horizontal, 8)
             .padding(.vertical, 4)
