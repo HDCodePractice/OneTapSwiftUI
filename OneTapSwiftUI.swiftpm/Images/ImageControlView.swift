03ce57f21d7894875c3865f1af0278a2e5da60f3
老房东 <huangdong@gmail.com>
2022-09-04 08:37:50 -0400

    fixed Image Tap

--- OneTapSwiftUI.swiftpm/Images/ImageControlView.swift
+++ OneTapSwiftUI.swiftpm/Images/ImageControlView.swift
@@ -2,48 +2,41 @@ import SwiftUI
 
 
 
-struct ImageView: View {
-    
-    
+struct ImageControlView: View {
     var body: some View {
-        
         VStack{
             HeadlineView(
                 title: "Image", 
                 url: "https://developer.apple.com/documentation/swiftui/image", 
                 description: "A view that displays an image."
             )
-            
             ImageViewCode()
-            Image(systemName: "sun.max")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .padding()
-            Text("sun")
-                .font(.largeTitle)
         }
     }
 }
 
 private struct ImageViewCode: View {
     var code = """
-    
 Image(systemName: "sun.max")
     .resizable()
     .aspectRatio(contentMode: .fit)
 Text("sun")
     .font(.largeTitle)
-        
-        
 """
     var body: some View {
         CodePreviewView(code: code)
+        Image(systemName: "sun.max")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .padding()
+        Text("sun")
+            .font(.largeTitle)
     }
 }
 
-struct ImageView_Previews: PreviewProvider {
+struct ImageControlView_Previews: PreviewProvider {
     static var previews: some View {
-        ImageView()
+        ImageControlView()
     }
 }
 
