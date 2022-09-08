88fd310a7b3387f8a285de40d88db77bd4ca8e58
老房东 <huangdong@gmail.com>
2022-09-07 20:14:03 -0400

    Update ButtonControlView.swift

--- OneTapSwiftUI.swiftpm/ControlsAndIndicators/ButtonControlView.swift
+++ OneTapSwiftUI.swiftpm/ControlsAndIndicators/ButtonControlView.swift
@@ -17,6 +17,12 @@ struct ButtonControlView: View {
     }
 }
 
+struct ButtonStyleSample: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
 private struct ButtonViewCode: View {
     var code = """
 struct ButtonViewCode: View {
@@ -33,23 +39,51 @@ struct ButtonViewCode: View {
                 Label("Button2",systemImage: "2.circle")
             }.buttonStyle(.borderedProminent)
         }
+            HStack{
+                Button("Delete Role", role: .destructive) { tapMessage = "Delete Role" }.buttonStyle(.borderedProminent)
+                Button("Cancel Role", role: .cancel) { tapMessage = "Cancel Role" }.buttonStyle(.borderedProminent)
+            }
+            HStack{
+                Button("automatic") { tapMessage = "automatic" }.buttonStyle(.automatic)
+                Button("bordered") { tapMessage = "bordered" }.buttonStyle(.bordered)
+            }
+            HStack{
+                Button("borderedProminent") { tapMessage = "borderedProminent" }.buttonStyle(.borderedProminent)
+                Button("borderless") { tapMessage = "borderless" }.buttonStyle(.borderless)
+            }
     }
 }
 """
     @State var tapMessage = "Tap a Button"
     var body: some View {
         CodePreviewView(code: code)
-        Text( tapMessage )
-        HStack{
-            Button("Button1"){
-                tapMessage = "Button1"
-            }.buttonStyle(.borderedProminent)
-            Button {
-                tapMessage = "Button2"
-            } label: {
-                Label("Button2",systemImage: "2.circle")
-            }.buttonStyle(.borderedProminent)
+        VStack{
+            Text( tapMessage )
+            HStack{
+                Button("Button1"){
+                    tapMessage = "Button1"
+                }.buttonStyle(.borderedProminent)
+                Button {
+                    tapMessage = "Button2"
+                } label: {
+                    Label("Button2",systemImage: "2.circle")
+                }.buttonStyle(.borderedProminent)
+            }
+            HStack{
+                Button("Delete Role", role: .destructive) { tapMessage = "Delete Role" }.buttonStyle(.borderedProminent)
+                Button("Cancel Role", role: .cancel) { tapMessage = "Cancel Role" }.buttonStyle(.borderedProminent)
+            }
+            HStack{
+                Button("automatic") { tapMessage = "automatic" }.buttonStyle(.automatic)
+                Button("bordered") { tapMessage = "bordered" }.buttonStyle(.bordered)
+            }
+            HStack{
+                Button("borderedProminent") { tapMessage = "borderedProminent" }.buttonStyle(.borderedProminent)
+                Button("borderless") { tapMessage = "borderless" }.buttonStyle(.borderless)
+            }
         }
+        .padding()
+        .border(.black)
     }
 }
 
