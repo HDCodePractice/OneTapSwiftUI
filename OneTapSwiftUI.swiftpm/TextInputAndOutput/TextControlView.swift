f2da159de4d770d2cc76ef79f818594b9870d73a
老房东 <huangdong@gmail.com>
2022-09-15 18:50:06 -0400

    Add DynamicScaleText Sample to Text Tap
    Co-Authored-By: 东坡肘子 <55673881+fatbobman@users.noreply.github.com>

--- OneTapSwiftUI.swiftpm/TextInputAndOutput/TextControlView.swift
+++ OneTapSwiftUI.swiftpm/TextInputAndOutput/TextControlView.swift
@@ -5,17 +5,21 @@ struct TextControlView: View {
         ScrollView{
             VStack{
                 HeadlineView(
-                    title: "Text", 
-                    url: "https://developer.apple.com/documentation/swiftui/text", 
+                    title: "Text",
+                    url: "https://developer.apple.com/documentation/swiftui/text",
                     description: "A view that displays one or more lines of read-only text."
                 )
-                TextFromStringView()
-                Divider()
-                TextForDateView()
-                Divider()
-                TextFormattingView()
-                Divider()
-                TextForOverview()
+                Group{
+                    TextFromStringView()
+                    Divider()
+                    TextForDateView()
+                    Divider()
+                    TextFormattingView()
+                    Divider()
+                    TextForOverview()
+                    Divider()
+                    DynamicScaleText()
+                }
                 Divider()
             }
             .padding()
@@ -23,6 +27,67 @@ struct TextControlView: View {
         .padding()
     }
 }
+
+// Create by Yang Xu on 2022/9/15
+// Modify by 老房东 on 2022/9/15
+struct DynamicScaleText: View {
+    var code: String {
+        return """
+VStack (alignment:. leading, spacing:20){
+    let longText = "A view that displays one or more lines of read-only text."
+    Text (longText)
+        .truncationMode (.tail)
+    Text (longText)
+        . truncationMode (.middle)
+    Text (longText)
+        .minimumScaleFactor(0.3)
+    if #available(iOS 16.0, *) {
+        ViewThatFits{
+            Text (longText)
+            Text ("A view that display read-only text view")
+            Text ("A view that display text")
+            Text ("Text view")
+                .minimumScaleFactor(0.3)
+        }
+    }
+}
+.lineLimit(1)
+.frame (width: \(String(format: "%.0f", width)))
+"""
+    }
+    @State var width: Double = 250
+    var body: some View{
+        VStack{
+            Text("Dynamic Scale Text")
+                .font(.title2)
+            CodePreviewView(code: code)
+            VStack (alignment:. leading, spacing:20){
+                let longText = "A view that displays one or more lines of read-only text."
+                Text (longText)
+                    .truncationMode (.tail)
+                Text (longText)
+                    . truncationMode (.middle)
+                Text (longText)
+                    .minimumScaleFactor(0.3)
+                if #available(iOS 16.0, *) {
+                    ViewThatFits{
+                        Text (longText)
+                        Text ("A view that display read-only text view")
+                        Text ("A view that display text")
+                        Text ("Text view")
+                            .minimumScaleFactor(0.3)
+                    }
+                }
+            }
+            .lineLimit(1)
+            .frame (width: width)
+            .border (.primary)
+            Slider (value: $width, in: 100...350)
+                .padding()
+        }
+    }
+}
+
 
 struct TextControlView_Previews: PreviewProvider {
     static var previews: some View {
