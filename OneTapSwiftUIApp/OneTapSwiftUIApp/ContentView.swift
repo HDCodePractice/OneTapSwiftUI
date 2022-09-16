8fd8067f570cfb79da92bfb75ab0f0bb55392c3a
老房东 <huangdong@gmail.com>
2022-09-16 18:48:12 -0400

    add a project file that use to publish to App Store

---
+++ OneTapSwiftUIApp/OneTapSwiftUIApp/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  OneTapSwiftUIApp
+//
+//  Created by 老房东 on 2022-09-16.
+//
+
+import SwiftUI
+import SwiftUITapsPackage
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView{
+            Sidebar()
+            Text("")
+        }
+        .navigationViewStyle(.columns)
+        .navigationTitle("")
+        .navigationBarHidden(true)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OneTapSwiftUIApp/OneTapSwiftUIApp/OneTapSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneTapSwiftUIAppApp.swift
+//  OneTapSwiftUIApp
+//
+//  Created by 老房东 on 2022-09-16.
+//
+
+import SwiftUI
+
+@main
+struct OneTapSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
