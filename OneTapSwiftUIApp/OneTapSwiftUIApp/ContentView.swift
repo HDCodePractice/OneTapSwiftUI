02e0dfff62782e7c9f788bb582197ce09e442194
老房东 <huangdong@gmail.com>
2022-09-23 07:51:02 -0400

    完成Animatable的中文化

--- OneTapSwiftUIApp/OneTapSwiftUIApp/ContentView.swift
+++ OneTapSwiftUIApp/OneTapSwiftUIApp/ContentView.swift
@@ -24,5 +24,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environment(\.locale, .init(identifier: "zh-CN"))
     }
 }
