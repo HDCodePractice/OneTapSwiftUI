77b80a87f13d8a597ebd1b265de48617276a665b
老房东 <huangdong@gmail.com>
2022-10-05 09:01:04 -0400

    add show Gestures

--- OneTapSwiftUIApp/OneTapSwiftUIApp/ContentView.swift
+++ OneTapSwiftUIApp/OneTapSwiftUIApp/ContentView.swift
@@ -5,21 +5,39 @@
 //  Created by 老房东 on 2022-09-21.
 //
 
+import ShowTime
 import SwiftUI
 import SwiftUITapsPackage
 
 struct ContentView: View {
+    @AppStorage("isShowTime") var isShowGestures: Bool = false
+
     var body: some View {
         NavigationView {
             Sidebar()
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        NavigationLink {
+                            SettingsView()
+                        } label: {
+                            Image(systemName: "ellipsis.circle")
+                        }
+                    }
+                }
             Text("")
+        }
+        .onAppear {
+            if isShowGestures{
+                ShowTime.enabled = .always
+            }else{
+                ShowTime.enabled = .never
+            }
         }
         .navigationViewStyle(.columns)
         .navigationTitle("")
         .navigationBarHidden(true)
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
